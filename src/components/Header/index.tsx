bda140c88efd7cc0dd43686f3081c3c3b33bfc69
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-30 00:58:19 -0300

    add buttons and icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
-import { HeaderContainer } from './styles'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
 import coffeeLogo from './../../assets/coffe-delivery-logo.svg'
 
 export const Header = () => (
   <HeaderContainer>
     <div className="container">
       <img src={coffeeLogo} alt="" />
+
+      <HeaderButtonsContainer>
+        <HeaderButton variant="purple">
+          <MapPin size={20} weight="fill" />
+          Botucatu, SP
+        </HeaderButton>
+
+        <HeaderButton variant="yellow">
+          <ShoppingCart size={20} weight="fill" />
+        </HeaderButton>
+      </HeaderButtonsContainer>
     </div>
   </HeaderContainer>
 )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const HeaderContainer = styled.header`
   width: 100%;
@@ -13,4 +13,39 @@ export const HeaderContainer = styled.header`
     align-items: center;
     justify-content: space-between;
   }
+`
+export const HeaderButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+`
+
+interface HeaderButtonProps {
+  variant: 'purple' | 'yellow'
+}
+export const HeaderButton = styled.button<HeaderButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.4rem;
+  min-width: 3.68rem;
+  height: 3.68rem;
+  border-radius: 0.6rem;
+  border: none;
+  padding: 0 0.8rem;
+  position: relative;
+  font-size: ${({ theme }) => theme.textSizes['text-s']};
+
+  ${({ variant, theme }) => css`
+    background-color: ${theme.colors[`${variant}-light`]};
+    color: ${theme.colors[`${variant}-dark`]};
+  `}
+
+  ${({ variant, theme }) =>
+    variant === 'purple' &&
+    css`
+      svg {
+        color: ${theme.colors.purple};
+      }
+    `}
 `
