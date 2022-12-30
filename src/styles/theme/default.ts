098bb0a6383a36551acf306d878a1758d8cf0946
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-29 23:16:15 -0300

    add defaultTheme

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,42 @@
+export const defaultTheme = {
+  colors: {
+    "white": "#FFFFFF",
+    "background": "#FAFAFA",
+    
+    "base-card": "#F3F2F2",
+    "base-input": "#EDEDED",
+    "base-button": "#E6E5E5",
+    "base-hover": "#D7D5D5",
+    "base-label": "#8D8686",
+    "base-text": "#574F4D",
+    "base-subtitle": "#403937",
+    "base-title": "#272221",
+
+    "purple-dark": "#4B2995",
+    "purple": "#8047F8",
+    "purple-light": "#EBE5F9",
+
+    "yellow-dark": "#C47F17",
+    "yellow": "#DBAC2C",
+    "yellow-light": "#F1E9C9",
+  },
+  textSizes: {
+    "title-xl": "4.8rem",
+    "title-l": "3.2rem",
+    "title-m": "2.4rem",
+    "title-s": "1.92rem",
+    "title-xs": "1.8rem",
+
+    "text-xl2": "2.4rem",
+    "text-xl": "2rem",
+    "text-l": "1.8rem",
+    "text-m": "1.6rem",
+    "text-s": "1.4rem",
+    "text-xs": "1.2rem",
+    "text-xs2": "1rem",
+  },
+  fonts: {
+    regular: "'Roboto'",
+    title: "'Baloo 2'",
+  },
+};
