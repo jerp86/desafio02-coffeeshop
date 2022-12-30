859eea5d9f9e0b11e5d6d78c50930ced2059247f
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-29 23:16:54 -0300

    add GlobalStyle

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.textSizes["text-m"]}
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
