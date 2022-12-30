15cd7719a08384f0012d5d4f3f7b5a95129b0fb1
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-30 00:33:03 -0300

    add header

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from '../../components/Header'
 import { LayoutContainer } from './styles'
 
 export const DefaultLayout = () => (
   <LayoutContainer>
-    <h2>Header</h2>
+    <Header />
     <Outlet />
   </LayoutContainer>
 )
