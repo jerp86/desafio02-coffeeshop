30cace39667bd1e576960c2947a54dc2f6fddee5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2022-12-30 00:16:34 -0300

    add layout

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import { CompleteOrder, Home } from "./pages";
+import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { CompleteOrder, Home } from './pages'
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/completeOrder" element={<CompleteOrder />} />
+    <Route path="/" element={<DefaultLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/completeOrder" element={<CompleteOrder />} />
+    </Route>
   </Routes>
-);
+)
