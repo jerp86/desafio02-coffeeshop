a6fa8083178b5a976ad71c4d44ec2230755897ef
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 17:44:43 -0300

    fix input number style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,4 +31,14 @@ export const GlobalStyle = createGlobalStyle`
   a {
     text-decoration: none;
   }
+  
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
 `
