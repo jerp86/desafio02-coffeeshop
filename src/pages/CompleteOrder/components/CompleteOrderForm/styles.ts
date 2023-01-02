64c32e592b377bdcbf695d8920eccb3121dfa92c
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 19:02:34 -0300

    create AddressFormContainer style

--- src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
+++ src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
@@ -13,3 +13,25 @@ export const FormSectionContainer = styled(SectionBaseStyle)`
   flex-direction: column;
   gap: 3.2rem;
 `
+
+export const AddressFormContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 20rem 28rem 6rem;
+  column-gap: 1.2rem;
+  row-gap: 1.6rem;
+  grid-auto-flow: dense;
+
+  .cep {
+    grid-column: span 3;
+    max-width: 20rem;
+  }
+
+  .street {
+    grid-column: span 3;
+  }
+
+  .complement {
+    grid-column: span 2;
+  }
+`
