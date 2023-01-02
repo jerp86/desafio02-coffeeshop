ae664083885f32b74412e727480aa4f4b3ab0897
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 11:13:01 -0300

    create OurCoffee page

---
+++ src/pages/OurCoffee/index.tsx
@@ -0,0 +1,10 @@
+import { TitleText } from '../../components'
+import { OurCoffeeContainer } from './styles'
+
+export const OurCoffee = () => (
+  <OurCoffeeContainer className="container">
+    <TitleText size="l" color="subtitle">
+      Nossos Cafés
+    </TitleText>
+  </OurCoffeeContainer>
+)
---
+++ src/pages/OurCoffee/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const OurCoffeeContainer = styled.section`
+  width: 100%;
+  margin-top: 3.2rem;
+`
