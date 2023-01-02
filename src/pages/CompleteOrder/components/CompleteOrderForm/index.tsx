66445523135978836900f547f6511202ab4dd983
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 18:44:22 -0300

    add SectionTitle

--- src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,10 +1,25 @@
+import { MapPinLine } from 'phosphor-react'
+import { useTheme } from 'styled-components'
 import { TitleText } from '../../../../components'
-import { CompleteOrderFormContainer } from './styles'
+import { SectionTitle } from '../SectionTitle'
+import { CompleteOrderFormContainer, FormSectionContainer } from './styles'
 
-export const CompleteOrderForm = () => (
-  <CompleteOrderFormContainer>
-    <TitleText size="xs" color="subtitle">
-      Complete seu pedido
-    </TitleText>
-  </CompleteOrderFormContainer>
-)
+export const CompleteOrderForm = () => {
+  const { colors } = useTheme()
+
+  return (
+    <CompleteOrderFormContainer>
+      <TitleText size="xs" color="subtitle">
+        Complete seu pedido
+      </TitleText>
+
+      <FormSectionContainer>
+        <SectionTitle
+          title="Endereço de Entrega"
+          subtitle="Informe o endereço onde deseja receber seu pedido"
+          icon={<MapPinLine size={22} color={colors['yellow-dark']} />}
+        />
+      </FormSectionContainer>
+    </CompleteOrderFormContainer>
+  )
+}
