59e1820565bf972b1bfa5b90790745872c788988
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 11:13:08 -0300

    add OurCoffee

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
+import { OurCoffee } from '../OurCoffee'
 import { Intro } from './components/Intro'
 import { HomeContainer } from './styles'
 
 export const Home = () => (
   <HomeContainer>
     <Intro />
+
+    <OurCoffee />
   </HomeContainer>
 )
