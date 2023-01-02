953774275dd6852c759eea572b3cdb5d3e71ae36
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 18:29:30 -0300

    create CompleteOrderForm component

---
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -0,0 +1,10 @@
+import { TitleText } from '../../../../components'
+import { CompleteOrderFormContainer } from './styles'
+
+export const CompleteOrderForm = () => (
+  <CompleteOrderFormContainer>
+    <TitleText size="xs" color="subtitle">
+      Complete seu pedido
+    </TitleText>
+  </CompleteOrderFormContainer>
+)
