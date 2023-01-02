1ee3c13610605b09fa267330cd2bb0deb66e8fd5
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 10:42:21 -0300

    add structure

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -1,9 +1,35 @@
-import { IntroContainer, IntroContent } from './styles'
+import {
+  BenefitsContainer,
+  IntroContainer,
+  IntroContent,
+  IntroTitle,
+} from './styles'
+import introImg from './../../../../assets/intro-img.png'
+import { RegularText } from '../../../../components'
 
 export const Intro = () => (
   <IntroContainer>
     <IntroContent className="container">
-      <p>Hello World</p>
+      <div>
+        <section>
+          <IntroTitle size="xl">
+            Encontre o café perfeito para qualquer hora do dia
+          </IntroTitle>
+
+          <RegularText>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </RegularText>
+        </section>
+
+        <BenefitsContainer>
+          <p>beneficio</p>
+          <p>beneficio</p>
+          <p>beneficio</p>
+          <p>beneficio</p>
+        </BenefitsContainer>
+      </div>
+      <img src={introImg} alt="" />
     </IntroContent>
   </IntroContainer>
 )
