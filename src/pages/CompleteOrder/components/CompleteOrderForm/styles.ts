46fe5c130956e63f6281e72d5f1417995b230b68
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 18:35:55 -0300

    create FormSectionContainer style

--- src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
+++ src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
+import { SectionBaseStyle } from '../../style'
 
 export const CompleteOrderFormContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.2rem;
   width: 64rem;
+`
+
+export const FormSectionContainer = styled(SectionBaseStyle)`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `
