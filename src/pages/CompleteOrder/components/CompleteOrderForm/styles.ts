462a188f80252eaf8d4c988af08f946e1aacb324
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-02 18:34:29 -0300

    add style in CompleteOrderFormContainer

--- src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
+++ src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
-export const CompleteOrderFormContainer = styled.div``
+export const CompleteOrderFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  width: 64rem;
+`
