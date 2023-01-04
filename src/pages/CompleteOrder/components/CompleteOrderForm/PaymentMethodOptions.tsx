e3952deef499f0d2b5018222c0c8cb30b5852bcf
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-04 19:22:31 -0300

    create PaymentMethodOptions

---
+++ src/pages/CompleteOrder/components/CompleteOrderForm/PaymentMethodOptions.tsx
@@ -0,0 +1,7 @@
+import { PaymentMethodOptionsContainer } from './styles'
+
+export const PaymentMethodOptions = () => (
+  <PaymentMethodOptionsContainer>
+    <h1>hello wordl</h1>
+  </PaymentMethodOptionsContainer>
+)
