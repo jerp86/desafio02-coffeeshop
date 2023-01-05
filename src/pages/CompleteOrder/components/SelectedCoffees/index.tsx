db95e9306ac46f15ded66fa9a09e2835328a8680
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-05 12:01:57 -0300

    create SelectedCoffees

---
+++ src/pages/CompleteOrder/components/SelectedCoffees/index.tsx
@@ -0,0 +1,14 @@
+import { TitleText } from '../../../../components'
+import { DetailsContainer, SelectedCoffeesContainer } from './styles'
+
+export const SelectedCoffees = () => (
+  <SelectedCoffeesContainer>
+    <TitleText>Cafés selecionados</TitleText>
+
+    <DetailsContainer>
+      <p>itens</p>
+      <p>itens</p>
+      <p>itens</p>
+    </DetailsContainer>
+  </SelectedCoffeesContainer>
+)
---
+++ src/pages/CompleteOrder/components/SelectedCoffees/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { SectionBaseStyle } from '../../style'
+
+export const SelectedCoffeesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  width: 64rem;
+`
+
+export const DetailsContainer = styled(SectionBaseStyle)`
+  border-radius: 0.6rem 4.4rem;
+  display: flex;
+  flex-direction: column;
+`
