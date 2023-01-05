7dfd758bd97b17fa80347e6217eb1f8652c809f3
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-05 12:16:37 -0300

    add style in CoffeeCartCardContainer

--- src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
+++ src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
@@ -1,6 +1,30 @@
 import styled from 'styled-components'
 
-export const CoffeeCartCardContainer = styled.div``
+export const CoffeeCartCardContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid ${({ theme }) => theme.colors['base-button']};
+  padding-bottom: 2.4rem;
+  margin-bottom: 2.4rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+
+    img {
+      width: 6.4rem;
+      height: 6.4rem;
+    }
+  }
+
+  > p {
+    font-weight: 700;
+    align-self: flex-start;
+  }
+`
 
 export const ActionsContainer = styled.div``
 
