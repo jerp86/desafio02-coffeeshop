d1631cd82490a644524218bf0600187c6d290041
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-05 12:02:53 -0300

    add SelectedCoffees

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,8 +1,11 @@
 import { CompleteOrderForm } from './components/CompleteOrderForm'
+import { SelectedCoffees } from './components/SelectedCoffees'
 import { CompleteOrderContainer } from './style'
 
 export const CompleteOrder = () => (
   <CompleteOrderContainer className="container">
     <CompleteOrderForm />
+
+    <SelectedCoffees />
   </CompleteOrderContainer>
 )
