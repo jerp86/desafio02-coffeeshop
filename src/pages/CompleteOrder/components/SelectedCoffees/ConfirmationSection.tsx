0968f6e78ef84f3fba0244d29b67ffd6070de05c
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-05 12:48:19 -0300

    create ConfirmationSection

---
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -0,0 +1,25 @@
+import { RegularText } from '../../../../components'
+import { ConfirmationSectionContainer } from './styles'
+
+export const ConfirmationSection = () => (
+  <ConfirmationSectionContainer>
+    <div>
+      <RegularText size="s">Total de itens</RegularText>
+      <RegularText>R$ 9,90</RegularText>
+    </div>
+
+    <div>
+      <RegularText size="s">Entrega</RegularText>
+      <RegularText>R$ 3,50</RegularText>
+    </div>
+
+    <div>
+      <RegularText size="l" weight={700} color="subtitle">
+        Total de itens
+      </RegularText>
+      <RegularText size="l" weight={700} color="subtitle">
+        R$ 29,90
+      </RegularText>
+    </div>
+  </ConfirmationSectionContainer>
+)
