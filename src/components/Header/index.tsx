d4775f68d3515bf18c13784570fb8473bfaf9923
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-09 18:09:55 -0300

    add span with cartQuantity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,26 +2,32 @@ import { MapPin, ShoppingCart } from 'phosphor-react'
 import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
 import coffeeLogo from './../../assets/coffee-delivery-logo.svg'
 import { NavLink } from 'react-router-dom'
+import { useCart } from '../../contexts/CartContext'
 
-export const Header = () => (
-  <HeaderContainer>
-    <div className="container">
-      <NavLink to="/">
-        <img src={coffeeLogo} alt="" />
-      </NavLink>
+export const Header = () => {
+  const { cartQuantity } = useCart()
 
-      <HeaderButtonsContainer>
-        <HeaderButton variant="purple">
-          <MapPin size={20} weight="fill" />
-          Botucatu, SP
-        </HeaderButton>
-
-        <NavLink to="/completeOrder">
-          <HeaderButton variant="yellow">
-            <ShoppingCart size={20} weight="fill" />
-          </HeaderButton>
+  return (
+    <HeaderContainer>
+      <div className="container">
+        <NavLink to="/">
+          <img src={coffeeLogo} alt="" />
         </NavLink>
-      </HeaderButtonsContainer>
-    </div>
-  </HeaderContainer>
-)
+
+        <HeaderButtonsContainer>
+          <HeaderButton variant="purple">
+            <MapPin size={20} weight="fill" />
+            Botucatu, SP
+          </HeaderButton>
+
+          <NavLink to="/completeOrder">
+            <HeaderButton variant="yellow">
+              {cartQuantity >= 1 && <span>{cartQuantity}</span>}
+              <ShoppingCart size={20} weight="fill" />
+            </HeaderButton>
+          </NavLink>
+        </HeaderButtonsContainer>
+      </div>
+    </HeaderContainer>
+  )
+}
