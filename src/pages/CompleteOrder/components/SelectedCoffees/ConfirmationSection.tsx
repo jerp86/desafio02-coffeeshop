14f0e538cd1704a7ebc82abeeb5abee4a9de321f
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-09 19:19:09 -0300

    add useCart

--- src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -1,27 +1,40 @@
 import { Button, RegularText } from '../../../../components'
+import { useCart } from '../../../../contexts/CartContext'
+import { formatNumber } from '../../../../utils/formatNumber'
 import { ConfirmationSectionContainer } from './styles'
 
-export const ConfirmationSection = () => (
-  <ConfirmationSectionContainer>
-    <div>
-      <RegularText size="s">Total de itens</RegularText>
-      <RegularText>R$ 9,90</RegularText>
-    </div>
+const DELIVERY_PRICE = 3.5
 
-    <div>
-      <RegularText size="s">Entrega</RegularText>
-      <RegularText>R$ 3,50</RegularText>
-    </div>
+export const ConfirmationSection = () => {
+  const { cartItemsTotal, cartQuantity } = useCart()
 
-    <div>
-      <RegularText size="l" weight={700} color="subtitle">
-        Total de itens
-      </RegularText>
-      <RegularText size="l" weight={700} color="subtitle">
-        R$ 29,90
-      </RegularText>
-    </div>
+  const cartTotal = cartQuantity > 0 ? DELIVERY_PRICE + cartItemsTotal : 0
+  const formattedItemsTotal = formatNumber(cartItemsTotal)
+  const formattedCartTotal = formatNumber(cartTotal)
+  const formattedDeliveryPrice = formatNumber(cartQuantity ? DELIVERY_PRICE : 0)
 
-    <Button text="confirmar pedido" />
-  </ConfirmationSectionContainer>
-)
+  return (
+    <ConfirmationSectionContainer>
+      <div>
+        <RegularText size="s">Total de itens</RegularText>
+        <RegularText>R$ {formattedItemsTotal}</RegularText>
+      </div>
+
+      <div>
+        <RegularText size="s">Entrega</RegularText>
+        <RegularText>R$ {formattedDeliveryPrice}</RegularText>
+      </div>
+
+      <div>
+        <RegularText size="l" weight={700} color="subtitle">
+          Total de itens
+        </RegularText>
+        <RegularText size="l" weight={700} color="subtitle">
+          R$ {formattedCartTotal}
+        </RegularText>
+      </div>
+
+      <Button text="confirmar pedido" disabled={cartQuantity <= 0} />
+    </ConfirmationSectionContainer>
+  )
+}
