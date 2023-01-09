ecc4e1dd6202b7e2605c15ee49e53a68182ef135
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-09 19:03:24 -0300

    add CoffeeCartCardProps and useCart

--- src/pages/CompleteOrder/components/CoffeeCartCard/index.tsx
+++ src/pages/CompleteOrder/components/CoffeeCartCard/index.tsx
@@ -1,31 +1,54 @@
 import { Trash } from 'phosphor-react'
 import { QuantityInput, RegularText } from '../../../../components'
+import { CartItem, useCart } from '../../../../contexts/CartContext'
+import { formatNumber } from '../../../../utils/formatNumber'
 import {
   ActionsContainer,
   CoffeeCartCardContainer,
   RemoveButton,
 } from './styles'
 
-export const CoffeeCartCard = () => (
-  <CoffeeCartCardContainer>
-    <div>
-      <img
-        src="https://s3-alpha-sig.figma.com/img/55b1/f9ee/64600f98b2bae456b96fdc624c4b4f47?Expires=1673827200&Signature=YMW9UdOQBV5K80PbKrm~-RqQCeCdwq0oRVdbJwBUiR49~o40KjW3Lrf-SWo-Vv7hvk2P3RUbb8QP99ldgAkvaWPzFEW9l24DZWbkD9c-mE-Jh5tyszzsJLkVv8gDTT7dH2zvSOeRz6MiCo76id6vIJSIBk490EB7w7cEhQrGL9aK3W3A-YcJ~e0vHmvjCOeFq3gEVW4~AhGVCsvJIE18JC1afJxiX-a69ZobChMFA1gU3wXOoKY-b-lZ3N8ng6Sr8dU1m7Gv6DimquEybyo9xez1wjhrosduH-55JF1SqR~0Sx-Ugyx9P215G3blazqLjZg6Kjh3rJmrEDV9~D6Frw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        alt=""
-      />
+interface CoffeeCartCardProps {
+  coffee: CartItem
+}
 
+export const CoffeeCartCard = ({ coffee }: CoffeeCartCardProps) => {
+  const { changeCartItemQuantity, removeCartItem } = useCart()
+
+  const coffeeTotal = coffee.price * coffee.quantity
+  const formattedPrice = formatNumber(coffeeTotal)
+
+  const handleDecrease = () =>
+    changeCartItemQuantity({ itemId: coffee.id, type: 'decrease' })
+
+  const handleIncrease = () =>
+    changeCartItemQuantity({ itemId: coffee.id, type: 'increase' })
+
+  const handleRemove = () => removeCartItem(coffee.id)
+
+  return (
+    <CoffeeCartCardContainer>
       <div>
-        <RegularText color="subtitle">Expresso Tradicional</RegularText>
-        <ActionsContainer>
-          <QuantityInput size="small" />
-          <RemoveButton>
-            <Trash size={16} />
-            Remover
-          </RemoveButton>
-        </ActionsContainer>
-      </div>
-    </div>
+        <img src={`/coffees/${coffee.photo}`} alt={coffee.name} />
 
-    <p>R$ 9,90</p>
-  </CoffeeCartCardContainer>
-)
+        <div>
+          <RegularText color="subtitle">{coffee.name}</RegularText>
+          <ActionsContainer>
+            <QuantityInput
+              size="small"
+              quantity={coffee.quantity}
+              onDecrease={handleDecrease}
+              onIncrease={handleIncrease}
+            />
+            <RemoveButton onClick={handleRemove}>
+              <Trash size={16} />
+              Remover
+            </RemoveButton>
+          </ActionsContainer>
+        </div>
+      </div>
+
+      <p>R$ {formattedPrice}</p>
+    </CoffeeCartCardContainer>
+  )
+}
