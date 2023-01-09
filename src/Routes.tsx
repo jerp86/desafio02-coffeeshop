dbb609995118d5f07002de9b3ca5204832cd067e
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-09 17:07:23 -0300

    add OrderConfirmed

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { CompleteOrder, Home } from './pages'
+import { CompleteOrder, Home, OrderConfirmed } from './pages'
 
 export const Router = () => (
   <Routes>
     <Route path="/" element={<DefaultLayout />}>
       <Route path="/" element={<Home />} />
       <Route path="/completeOrder" element={<CompleteOrder />} />
+      <Route path="/orderConfirmed" element={<OrderConfirmed />} />
     </Route>
   </Routes>
 )
